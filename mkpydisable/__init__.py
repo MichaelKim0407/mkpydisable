c41dff8b2a88270bdef47971390bff98941a6ce4
Michael Kim <mkim0407@gmail.com>
2016-10-03 14:49:00 +0800

    import

--- mkpydisable/__init__.py
+++ mkpydisable/__init__.py
@@ -1,12 +1,17 @@
 __author__ = 'Michael'
 
 
-def cleanup(globals, locals):
+def cleanup(globals, locals, *exceptions):
     for key in globals.keys():
+        if globals[key] in exceptions:
+            continue
         globals.pop(key)
     for key in locals.keys():
+        if locals[key] in exceptions:
+            continue
         locals.pop(key)
 
 
 from . import builtins
+from . import import_
 from . import open
